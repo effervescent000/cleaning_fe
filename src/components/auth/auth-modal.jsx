fe117f1ddf231fa5e3c0ea22b70955cf227e4f6f
effervescent000 <kislio01@gmail.com>
2022-06-02 07:12:15 -0400

    c/p auth form from other project

--- src/components/auth/auth-modal.jsx
+++ src/components/auth/auth-modal.jsx
@@ -1,10 +1,46 @@
 import React from "react";
-import { Modal, ModalBody, ModalHeader, ModalFooter } from "reactstrap";
+import { useDispatch } from "react-redux";
+import { Form } from "react-final-form";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
-const AuthModal = ({ isOpen, toggle, newUser }) => {
+import APIService from "../../utils/api-service";
+import { urls } from "../../constants/constants";
+import { userConstants } from "../../constants/user.constants";
+
+import TextInputField from "../form-components/text-input";
+
+const AuthModal = ({ newUser, toggle, isOpen }) => {
+  const dispatch = useDispatch();
+
+  const onSubmit = (values) => {
+    const callback = (response) =>
+      dispatch({ type: userConstants.SET_USER, payload: response.data });
+    APIService.POST(newUser ? urls.SIGNUP : urls.LOGIN, values, callback);
+  };
+
   return (
     <Modal isOpen={isOpen} toggle={toggle}>
       <ModalHeader>{newUser ? "Register" : "Login"}</ModalHeader>
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <ModalBody>
+              <TextInputField name="username" label="Username" />
+              <TextInputField name="password" label="Password" />
+              {newUser && <TextInputField name="confirmPassword" label="Password" />}
+            </ModalBody>
+            <ModalFooter>
+              <div className="buttons-wrapper">
+                <button type="button" onClick={toggle}>
+                  Cancel
+                </button>
+                <button type="submit">Submit</button>
+              </div>
+            </ModalFooter>
+          </form>
+        )}
+      />
     </Modal>
   );
 };
