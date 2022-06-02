53c5e8f6e52b48d2c5105b964377bf180060d746
effervescent000 <kislio01@gmail.com>
2022-06-02 08:59:57 -0400

    added components for displaying rooms

---
+++ src/components/rooms/room-card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoomCard = ({ room }) => {
+  return <div>{room.label}</div>;
+};
+
+export default RoomCard;
--- src/components/rooms/room-page.jsx
+++ src/components/rooms/room-page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const RoomPage = () => {
+  const rooms = useSelector((state) => state.rooms);
+
   return (
     <div>
-      {/* ooooooo */}
-      Nothing here yet
-      {/* spooky */}
+      {rooms.length ? <>Nothing here yet!</> : <>No rooms added!</>}
+      <button>+</button>
     </div>
   );
 };
