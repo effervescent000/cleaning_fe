e082c24d4e392e080ec8adecee2550e73f5b7624
effervescent000 <kislio01@gmail.com>
2022-06-02 07:40:51 -0400

    setting up rooms in redux

---
+++ src/constants/rooms.constants.js
@@ -0,0 +1,5 @@
+export const roomConstants = {
+  SET_ROOMS: "rooms/setRooms",
+  ADD_ROOM: "rooms/addRoom",
+  REMOVE_ROOM: "rooms/removeRoom",
+};
--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "./reducers/user";
+import roomsReducer from "./reducers/rooms";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    rooms: roomsReducer,
   },
 });
 
