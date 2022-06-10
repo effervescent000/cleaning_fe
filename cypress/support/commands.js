37c302bbd016e4cb02561fbc39a5b6d6a4bf1083
effervescent000 <kislio01@gmail.com>
2022-06-10 08:23:50 -0400

    adding cypress and making a login test

---
+++ cypress/support/commands.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+Cypress.Commands.add("login", (username, password) => {
+  cy.contains("Auth").click();
+  cy.contains("Login").click();
+  cy.get("input[name=username]").type(username);
+  cy.get("input[name=password]").type(password);
+  cy.contains("Submit").click();
+});
+
+Cypress.Commands.add("logout", () => {
+  cy.contains("Auth").click();
+  cy.contains("Logout").click();
+});
+
+Cypress.Commands.add("navigateToRooms", () => {
+  const user = { username: "test_user", password: "ilovebutts" };
+  cy.visit(`http://127.0.0.1:3000`);
+  cy.login(user.username, user.password);
+  cy.contains("Rooms").click();
+});
