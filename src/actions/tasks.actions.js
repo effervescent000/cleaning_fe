de4bba35321d0d04a3d0deff072e258a7d125997
effervescent000 <kislio01@gmail.com>
2022-06-15 07:49:39 -0400

    added functional button to delete tasks

--- src/actions/tasks.actions.js
+++ src/actions/tasks.actions.js
@@ -1,4 +1,6 @@
+import { urls } from "../constants/constants";
 import { taskConstants } from "../constants/tasks.constants";
+import apiService from "../utils/api-service";
 
 export const setTasks = (tasks) => ({
   type: taskConstants.SET_TASKS,
@@ -19,3 +21,7 @@ export const replaceTask = (task) => ({
   type: taskConstants.REPLACE_TASK,
   payload: task,
 });
+
+export const deleteTaskAndUpdate = (id) => (dispatch) => {
+  apiService.DELETE(urls.ROOMS(id), (response) => dispatch(setTasks(response.data)));
+};
